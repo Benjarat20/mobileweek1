32d712db0dc2859d886eaff425e11753d44e94b5
Benjarat <s6302041510052@email.kmutnb.ac.th>
2021-09-16 18:26:54 +0700

    16

--- lib/screen/register.dart
+++ lib/screen/register.dart
@@ -1,22 +1,199 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:mobileappweek1/config/constant.dart';
 
 class Register extends StatefulWidget {
-  const Register({ Key? key }) : super(key: key);
+  const Register({Key? key}) : super(key: key);
 
   @override
   _RegisterState createState() => _RegisterState();
 }
 
 class _RegisterState extends State<Register> {
+  var name, surname, email, password, repassword;
+  final formKey = GlobalKey<FormState>();
+
+  final TextEditingController _pass = TextEditingController();
+  final TextEditingController _confirmPass = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Register'), 
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Register'),
+        ),
+        body: Form(
+          key: formKey,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                txtName(),
+                txtSurname(),
+                txtEmail(),
+                txtPassword(),
+                txtRePassword(),
+                btnSubmit(),
+              ],
+            ),
+          ),
+        ),
       ),
-      body: Text('Hello'),
-     
     );
   }
+
+  Widget txtName() {
+    return Container(
+      margin: EdgeInsets.fromLTRB(15, 20, 15, 20),
+      child: TextFormField(
+        style: TextStyle(
+          fontSize: 24,
+          color: pColor,
+        ),
+        decoration: InputDecoration(
+          labelText: 'Name:',
+          icon: Icon(Icons.add_reaction_sharp),
+          hintText: 'Input your name',
+        ),
+        validator: (value) {
+          if (value!.isEmpty) {
+            return 'กรุณาใส่ข้อมูลด้วย';
+          } else if (value.length < 2) {
+            return 'กรุณาใส่ข้อมูลมากกว่า 2 ตัวอักษร';
+          }
+        },
+        onSaved: (value) {
+          name = value;
+        },
+      ),
+    );
+  }
+
+  Widget txtSurname() {
+    return Container(
+      margin: EdgeInsets.fromLTRB(15, 20, 15, 20),
+      child: TextFormField(
+        style: TextStyle(
+          fontSize: 24,
+          color: pColor,
+        ),
+        decoration: InputDecoration(
+          labelText: 'Surname:',
+          icon: Icon(Icons.add_reaction_sharp),
+          hintText: 'Input your surname',
+        ),
+        validator: (value) {
+          if (value!.isEmpty) {
+            return 'กรุณากรอกข้อมูล';
+          }
+        },
+        onSaved: (value) {
+          surname = value!.trim();
+        },
+      ),
+    );
+  }
+
+  Widget txtEmail() {
+    return Container(
+      margin: EdgeInsets.fromLTRB(15, 20, 15, 20),
+      child: TextFormField(
+        keyboardType: TextInputType.emailAddress,
+        style: TextStyle(
+          fontSize: 24,
+          color: pColor,
+        ),
+        decoration: InputDecoration(
+          labelText: 'Email:',
+          icon: Icon(Icons.email),
+          hintText: 'Input your email',
+        ),
+        validator: (value) {
+          if (!(value!.contains('@'))) {
+            return 'กรุณากรอกข้อมูลตามรูปแบบอีเมลด้วย';
+          } else if (!(value.contains('.'))) {
+            return 'กรุณากรอกข้อมูลตามรูปแบบอีเมลด้วย';
+          }
+        },
+        onSaved: (value) {
+          email = value;
+        },
+      ),
+    );
+  }
+
+  Widget txtPassword() {
+    return Container(
+      margin: EdgeInsets.fromLTRB(15, 20, 15, 20),
+      child: TextFormField(
+        obscureText: true,
+        style: TextStyle(
+          fontSize: 24,
+          color: pColor,
+        ),
+        decoration: InputDecoration(
+          labelText: 'Password:',
+          icon: Icon(Icons.lock),
+          hintText: 'Input your password',
+        ),
+        controller: _pass,
+        validator: (value) {
+          if (value!.isEmpty) {
+            return "กรุณากรอก Password";
+            return null;
+          } else if (value.length < 8) {
+            return "กรุณากรอก Password อย่างน้อย 8 ตัว";
+          }
+        },
+        onSaved: (value) {
+          password = value;
+        },
+      ),
+    );
+  }
+
+  Widget txtRePassword() {
+    return Container(
+      margin: EdgeInsets.fromLTRB(15, 20, 15, 20),
+      child: TextFormField(
+        obscureText: true,
+        style: TextStyle(
+          fontSize: 24,
+          color: pColor,
+        ),
+        decoration: InputDecoration(
+          labelText: 'Re-Password:',
+          icon: Icon(Icons.lock),
+          hintText: 'Press your password Again',
+        ),
+        controller: _confirmPass,
+        validator: (value) {
+          if (value!.isEmpty) {
+            return "กรุณากรอก Password ให้ครบ";
+          } else if (value != _pass.text) {
+            return "กรุณากรอก Password อย่างน้อย 8 ตัว";
+            return null;
+          }
+        },
+        onSaved: (value) {
+          repassword = value;
+        },
+      ),
+    );
+  }
+
+  Widget btnSubmit() => ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          primary: pColor,
+        ),
+        onPressed: () {
+          print("Hello");
+          if (formKey.currentState!.validate()) {
+            formKey.currentState!.save();
+            print(
+                "Name : $name Surname : $surname Emai : $email Password : $password  RePassword : $repassword");
+            formKey.currentState!.reset();
+          }
+        },
+        child: Text('Submit'),
+      );
 }
